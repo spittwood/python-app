c79dac2ee9e5089e3e8df111841b6d280dfc914d
Stuart Pittwood <stuart.pittwood@arup.com>
2026-01-12 12:31:28 +0000

    push

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import datetime, socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify(
+        {
+            "message": "Hello Humans",
+            'hostname': socket.gethostname(),
+            "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+        }
+    )
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({"status": "up"}),200
+    
+
+if __name__ == '__main__':
+    
+    app.run(host="0.0.0.0")
