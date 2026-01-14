72c765af8a3838aec5a2adfbec6df12cbfa5f52f
Stuart Pittwood <36988660+spittwood@users.noreply.github.com>
2026-01-14 12:47:42 +0000

    Update message in details function response

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def details():
     return jsonify(
         {
-            "message": "Lorem Ipsum from Flask! again",
+            "message": "Lorem Ipsum from Flask!",
             'hostname': socket.gethostname(),
             "time": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
         }
